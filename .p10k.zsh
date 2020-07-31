72f26bbd49f6b4fa6a6edd49494febcb258203d5
Harsil Patel <harsilspatel@gmail.com>
2020-07-31 17:59:10 +1000

    fix(p10k): remove prev command duration

--- .p10k.zsh
+++ .p10k.zsh
@@ -58,7 +58,7 @@
 
   # Right prompt segments.
   typeset -g POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(
-    command_execution_time    # previous command duration
+    # command_execution_time    # previous command duration
     virtualenv                # python virtual environment
     context                   # user@host
     time                      # current time
