0a658c26af7f07f5a57800c301c6065bd84ef2c0
Harsil Patel <harsilspatel@gmail.com>
2022-02-12 19:52:47 +1100

    hammerspoon: hotkey extension

--- hammerspoon/hotkey-extension.lua
+++ hammerspoon/hotkey-extension.lua
@@ -17,45 +17,73 @@ local MODIFIER_FLAGS = {
 local bindings = {}
 
 local function getModifierNames(modifierFlags)
-  local flags = {}
-  for keyname, flag in pairs(MODIFIER_FLAGS) do if (modifierFlags & tonumber(flag) > 0) then flags[keyname] = true end end
-  print('flags', dump(flags))
-  return flags
-end
-
-local function compareModifiers(hotkeyModifierNames, eventModifierFlags)
-  -- hotkeyModifiers will be table of strings for e.g {'ralt', 'lcmd'}
-  -- eventModifierFlags will be an integer for e.g 524608 (i.e 'ralt')
-  local eventModifierNamesMap = getModifierNames(eventModifierFlags)
-  print('xxxxxxxxxxxxxx', dump(hotkeyModifierNames), dump(eventModifierNamesMap))
-  for _, modifier in ipairs(hotkeyModifierNames) do
-    local leftModifier = 'l' .. modifier
-    local rightModifier = 'r' .. modifier
-
-    local isModifierPressed = eventModifierNamesMap[modifier] or eventModifierNamesMap[leftModifier] or
-                                eventModifierNamesMap[rightModifier]
-    print('yyyy', modifier, isModifierPressed)
-    if (not isModifierPressed) then return false end
+  local names = {}
+  for keyname, flag in pairs(MODIFIER_FLAGS) do
+    if (modifierFlags & tonumber(flag) > 0) then table.insert(names, keyname) end
   end
-  return true
+  return names
 end
 
-local keyDown = hs.eventtap.new({hs.eventtap.event.types.keyDown}, function(event)
-  local eventData = event:getRawEventData()['NSEventData']
-  local keyCode = eventData.keyCode
-  local modifierFlags = eventData.modifierFlags
+local function addModifierToAllCombinations(combinations, modifierName)
+  return hs.fnutils.imap(combinations, function(combination)
+    table.insert(combination, modifierName) -- mutates combination i.e. doesn't return anything
+    return combination
+  end)
+end
 
-  for _, binding in ipairs(bindings) do
-    local key = binding.key
-    local mods = binding.mods
-    -- local pressedFn = binding.pressedFn
-    if (key == hs.keycodes.map[keyCode] and compareModifiers(mods, modifierFlags)) then
-      binding.pressedFn()
-      return true
+local function getAllModifierCombinations(hotkeyModifierNames)
+  local combinations = {{}}
+
+  for _, modifierName in ipairs(hotkeyModifierNames) do
+    local leftModifier = 'l' .. modifierName
+    local rightModifier = 'r' .. modifierName
+    local isCommonModifier = MODIFIER_FLAGS[leftModifier] and MODIFIER_FLAGS[rightModifier]
+    if isCommonModifier then
+      local combinationsClone = hs.fnutils.imap(combinations,
+        function(combination) return {table.unpack(combination)} end)
+      combinationsClone = addModifierToAllCombinations(combinationsClone, leftModifier)
+
+      combinations = addModifierToAllCombinations(combinations, rightModifier)
+      combinations = hs.fnutils.concat(combinations, combinationsClone)
+    else
+      combinations = addModifierToAllCombinations(combinations, modifierName)
     end
+
   end
 
-  print(dump(bindings))
+  return combinations
+end
+
+local function getHotkeyHash(modifiers, key)
+  -- sorting combinations so "hash" is deterministic
+  table.sort(modifiers)
+  table.insert(modifiers, key)
+  return table.concat(modifiers, '.')
+end
+
+local keyDownEventTap = hs.eventtap.new({hs.eventtap.event.types.keyDown}, function(event)
+  local eventData = event:getRawEventData()['NSEventData']
+
+  local keyCode = eventData.keyCode
+  local keyName = hs.keycodes.map[keyCode]
+
+  local modifierFlags = eventData.modifierFlags
+  local eventModifiers = getModifierNames(modifierFlags)
+
+  p(eventModifiers)
+  local eventHotkeyHash = getHotkeyHash(eventModifiers, keyName)
+  print('yo', eventHotkeyHash)
+  print('pressedFn', bindings[eventHotkeyHash])
+
+  if #eventModifiers == 0 or (#eventModifiers == 1 and hs.fnutils.contains({'lcmd', 'rcmd'}, eventModifiers[1])) then
+    return
+  elseif (bindings[eventHotkeyHash]) then
+    local pressedFn = bindings[eventHotkeyHash]
+    pressedFn(eventData)
+    return true
+  end
+
+  -- print(dump(bindings))
   -- getModifierNames(modifierFlags)
   -- if (hs.keycodes.map[keyCode] == key and modifierFlags == 524608) then hs.application.launchOrFocus('Slack') end
 end)
@@ -66,9 +94,18 @@ function module.bind(mods, key, pressedFn)
   -- if (not bindings[key]) then bindings[key] = {} end
   -- local keyTable = bindings[key]
   -- keyTable[#keyTable + 1] = {mods = mods, pressedFn = pressedFn}
-  bindings[#bindings + 1] = {mods = mods, key = key, pressedFn = pressedFn}
 
-  if (not keyDown:isEnabled()) then keyDown:start() end
+  local combinations = getAllModifierCombinations(mods)
+  local hotkeyHashes = hs.fnutils.imap(combinations, function(combination) return getHotkeyHash(combination, key) end)
+
+  hs.fnutils.ieach(hotkeyHashes, function(hotkeyHash) bindings[hotkeyHash] = pressedFn end)
+  print('dssfasdfasdfa', p(bindings))
+
+  -- print('hotkeyHashes', dump(hotkeyHashes))
+
+  -- bindings[#bindings + 1] = {mods = mods, key = key, pressedFn = pressedFn}
+
+  if (not keyDownEventTap:isEnabled()) then keyDownEventTap:start() end
 end
 
 hs.hotkeyextension = module
