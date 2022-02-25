0be73de71d1a49384c3747870cdeb42c1107674d
Harsil Patel <hpatel2@atlassian.com>
2022-02-25 11:08:47 +1100

    hammerspoon: refactor to spoon-manager

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,11 +1,7 @@
-hs.spoons.use('SpoonInstall')
-
-spoon.SpoonInstall:andUse('HoldToQuit', {start = true})
-spoon.SpoonInstall:andUse('ReloadConfiguration', {start = true})
-
 require 'utils'
 require 'hotkey-extension'
 
 require 'app-manager'
 require 'media-manager'
+require 'spoon-manager'
 require 'window-manager'
---
+++ hammerspoon/spoon-manager.lua
@@ -0,0 +1,4 @@
+hs.spoons.use('SpoonInstall')
+
+spoon.SpoonInstall:andUse('HoldToQuit', {start = true})
+spoon.SpoonInstall:andUse('ReloadConfiguration', {start = true})
