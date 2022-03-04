1258ef3e64ed70a73533d4ba4ea3085531f5d65f
Harsil Patel <harsilspatel@gmail.com>
2022-03-05 02:22:47 +1100

    hammerspoon: step audio vlc

--- hammerspoon/media-manager.lua
+++ hammerspoon/media-manager.lua
@@ -11,11 +11,22 @@ local function sendSystemKey(key)
   end
 end
 
+local function seekAudio(direction)
+  return function()
+    hs.osascript.applescript(
+      'set appName to "VLC"\nif application appName is running then\ntell application "VLC" to step ' .. direction ..
+        '\nend if\nend')
+  end
+end
+
 local mediaManagerModifiers = {'rcmd'}
 
 hs.hotkeyextension.bind(mediaManagerModifiers, 'q', sendSystemKey('PREVIOUS'))
 hs.hotkeyextension.bind(mediaManagerModifiers, 'w', sendSystemKey('PLAY'))
 hs.hotkeyextension.bind(mediaManagerModifiers, 'e', sendSystemKey('NEXT'))
+
+hs.hotkeyextension.bind(mediaManagerModifiers, 'a', seekAudio('backward'))
+hs.hotkeyextension.bind(mediaManagerModifiers, 's', seekAudio('forward'))
 
 hs.hotkeyextension.bind(mediaManagerModifiers, 'z', sendSystemKey('MUTE'))
 hs.hotkeyextension.bind(mediaManagerModifiers, 'x', sendSystemKey('SOUND_DOWN'))
