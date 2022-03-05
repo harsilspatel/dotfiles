0b4a7e3e7fb0dff420a0ab9dc0b8f22b638500e8
Harsil Patel <harsilspatel@gmail.com>
2022-03-05 23:13:52 +1100

    hammerspoon: spoon update cron; supplementary submodules

--- hammerspoon/app-manager.lua
+++ hammerspoon/app-manager.lua
@@ -1,11 +1,13 @@
 function launchOrFocus(app) return function() hs.application.launchOrFocus(app) end end
 
-local appManagerModifiers = { 'ralt' }
+local appManagerModifiers = {'ralt'}
 
 hs.hotkeyextension.bind(appManagerModifiers, 'r', launchOrFocus('System Preferences'))
 
 hs.hotkeyextension.bind(appManagerModifiers, 'a', launchOrFocus('Authy Desktop'))
-hs.hotkeyextension.bind(appManagerModifiers, 's', function() hs.osascript.applescript('tell application "/Applications/Google Chrome.app"\nmake new window\nactivate\nend tell') end)
+hs.hotkeyextension.bind(appManagerModifiers, 's', function()
+  hs.osascript.applescript('tell application "/Applications/Google Chrome.app"\nmake new window\nactivate\nend tell')
+end)
 hs.hotkeyextension.bind(appManagerModifiers, 'd', launchOrFocus('Slack'))
 hs.hotkeyextension.bind(appManagerModifiers, 'f', launchOrFocus('Spotify'))
 
@@ -13,12 +15,16 @@ hs.hotkeyextension.bind(appManagerModifiers, 'h', launchOrFocus('Hammerspoon'))
 
 hs.hotkeyextension.bind(appManagerModifiers, 'b', launchOrFocus('BetterTouchTool'))
 
-local UI = require('rb.ui')
+-- when we import rb.ui, it then tries to import it's sibling file utils.lua by
+-- importing rb.utils and local since it's not in path it seems to can't find it
+-- source: https://stackoverflow.com/a/9145447/9701238
+package.path = './modules/roeybiran/?.lua;' .. package.path
+local UI = require('modules.roeybiran.rb.ui')
 local ax = require('hs.axuielement')
 
-local function focusDock() UI.getUIElement(hs.application('Dock'), { { 'AXList', 1 } }):setAttributeValue('AXFocused', true) end
+local function focusDock() UI.getUIElement(hs.application('Dock'), {{'AXList', 1}}):setAttributeValue('AXFocused', true) end
 
-local function focusMenuBar() ax.systemElementAtPosition({ 0, 0 }):attributeValue('AXParent')[2]:performAction('AXPress') end
+local function focusMenuBar() ax.systemElementAtPosition({0, 0}):attributeValue('AXParent')[2]:performAction('AXPress') end
 
 hs.hotkeyextension.bind(appManagerModifiers, ',', focusMenuBar)
 hs.hotkeyextension.bind(appManagerModifiers, '.', focusDock)
--- hammerspoon/spoon-manager.lua
+++ hammerspoon/spoon-manager.lua
@@ -2,3 +2,9 @@ hs.spoons.use('SpoonInstall')
 
 spoon.SpoonInstall:andUse('HoldToQuit', {start = true})
 spoon.SpoonInstall:andUse('ReloadConfiguration', {start = true})
+
+local EVERY_SIX_HOURS = 60 * 60 * 6 -- seconds
+hs.timer.doWhile(function() return true end, function()
+  print('spoon-manager: updated all spoons! (ﾉ^_^)ﾉ')
+  spoon.SpoonInstall:asyncUpdateAllRepos()
+end, EVERY_SIX_HOURS)
