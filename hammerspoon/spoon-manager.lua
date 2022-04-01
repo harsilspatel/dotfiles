9fb1bee53142fb917c0f04c7617e48034ccf3369
Harsil Patel <hpatel2@atlassian.com>
2022-04-01 23:09:51 +1100

    hammerspoon: skyrocket for easier windows manipulations

--- hammerspoon/spoon-manager.lua
+++ hammerspoon/spoon-manager.lua
@@ -2,6 +2,14 @@ hs.spoons.use('SpoonInstall')
 
 spoon.SpoonInstall:andUse('ReloadConfiguration', {start = true})
 
+hs.loadSpoon('SkyRocket'):new({
+  opacity = 0.2, -- Opacity of resize canvas
+  moveModifiers = {'ctrl', 'cmd'}, -- Which modifiers to hold to move a window?
+  moveMouseButton = 'left', -- Which mouse button to hold to move a window?
+  resizeModifiers = {'fn'}, -- Which modifiers to hold to resize a window?
+  resizeMouseButton = 'left' -- Which mouse button to hold to resize a window?
+})
+
 local EVERY_SIX_HOURS = 60 * 60 * 6 -- seconds
 hs.timer.doWhile(function() return true end, function()
   print('spoon-manager: updated all spoons! (ﾉ^_^)ﾉ')
